b7ccf01f0e8b400be43c83301b7d758708fdb8fa
Agence Web La Capsule <132720142+Lacapsule66@users.noreply.github.com>
2023-08-01 00:00:59 +0200

    Add files via upload

--- app/components/Anim.jsx
+++ app/components/Anim.jsx
@@ -26,7 +26,7 @@ export default function Anim() {
   return (
     <>
       <div id="ok1" className="bg-[#ffe95c] h-screen w-screen absolute top-0 z-50" >
-        <video className="h-full w-full object-cover" autoPlay muted loop src="burgervideo.mp4"></video>
+        <video className="h-full w-full object-cover" autoPlay muted loop src="https://res.cloudinary.com/dcl00yipc/video/upload/v1690840597/burgervideo_hhbyg3.mp4"></video>
       </div>
       <div id="ok" className="bg-pink-500 absolute w-screen h-screen z-40"></div>
     </>
