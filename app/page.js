68dacf242aec563c2a95d66cc4c06027005a2d5c
Agence Web La Capsule <132720142+Lacapsule66@users.noreply.github.com>
2023-07-31 20:11:53 +0200

    Add files via upload

---
+++ app/page.js
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import First from "./components/First";
+import Last from "./components/Last";
+import ProductList from "./components/ProductList";
+
+export default function Home() {
+  return (
+    <>
+    
+      <First />
+      <ProductList />
+      <Last />
+    </>
+  );
+}
